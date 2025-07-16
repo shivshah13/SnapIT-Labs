807a96c752681fa989fe31a5eb5d3a1935eba172
shivshah13 <shahshiv206@gmail.com>
2025-07-16 16:05:38 -0400

    Create numberslab.js

---
+++ numberslab.js
@@ -0,0 +1,23 @@
+// task 1 – using numeric separators to make large numbers easier to read
+let a = 34_038;
+let b = 45_838.8734;
+let c = 0b0110_0010_1011;
+let d = 0x17_4C0;
+let e = -895_612;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+// task 2 – all of these should equal 29
+let decimal = 29;
+let hex = 0x1D;
+let binary = 0b11101;
+let octal = 0o35;
+
+console.log(decimal);
+console.log(hex);
+console.log(binary);
+console.log(octal);
