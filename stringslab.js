30ee8f41558c241b714268595d32baafd6fcf496
shivshah13 <shahshiv206@gmail.com>
2025-07-16 00:52:49 -0400

    Update stringslab.js

--- stringslab.js
+++ stringslab.js
@@ -1,1 +1,8 @@
+let sentence = "  This statement has empty spaces   ";
 
+console.log(sentence.includes("statement"));
+console.log(sentence.at(2));
+console.log(sentence.trim());
+console.log(sentence.toLowerCase());
+
+console.log("Jack and Jill went up the hill\nto fetch a pail of water.\nJack fell down and broke his crown\nand Jill came tumbling after.");
